0362399d2f5b267095a2929bd19ecb498df7001b
Eduardo Alvarez <opticoedu@gmail.com>
2023-11-21 14:18:52 -0300

    ultima revision

---
+++ AtividadeReact02/src/components/Menu/index.jsx
@@ -0,0 +1,19 @@
+
+import "./styles.css"
+
+export function Footer() {
+
+    return (
+
+        <div id="links">
+
+            <a target="_blank" href="#">Destinos</a>
+            
+            <a target="_blank" href="#">Ofertas</a>
+
+            <a href="mailto:opticoedu@gmail.com">Contato</a>
+
+            
+        </div>
+    )
+}
